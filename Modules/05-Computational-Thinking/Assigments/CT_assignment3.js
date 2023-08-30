c75b853bed1be33ddef61f32a01eb2355bda0915
ikhwannaufal <139196434+ikhwannaufal@users.noreply.github.com>
2023-08-30 21:13:07 +0800

    Add files via upload

---
+++ Modules/05-Computational-Thinking/Assigments/CT_assignment3.js
@@ -0,0 +1,29 @@
+function generation(gens, sex) {
+
+    const family = {
+        "-3" : ['great grandfather', 'great grandmother'],
+        "-2" : ['grandfather', 'grandmother'],
+        "-1" : ['father', 'mother'],
+        "0" : ['me!', 'me!'],
+        "1" : ['son', 'daughter'],
+        "2" : ['grandson', 'granddaughter'],
+        "3" : ['great grandson', 'great granddaughter'],
+    }
+
+    return sex === 'm' ? family[gens][0] : sex ==='f' ? family[gens][1] : undefined;
+}
+
+console.log(generation(-3, "m"))
+console.log(generation(-3, "f"))
+console.log(generation(-2, "m"))
+console.log(generation(-2, "f"))
+console.log(generation(-1, "m"))
+console.log(generation(-1, "f"))
+console.log(generation(0, "m"))
+console.log(generation(0, "f"))
+console.log(generation(1, "m"))
+console.log(generation(1, "f"))
+console.log(generation(2, "m"))
+console.log(generation(2, "f"))
+console.log(generation(3, "m"))
+console.log(generation(3, "f"))
